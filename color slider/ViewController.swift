9504bcb01fd26a9e496ed20c925d92e7903ec274
Yifan Cui <zjgdxbs@me.com>
2020-03-11 08:33:46 -0400

    Initial Commit

---
+++ color slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  color slider
+//
+//  Created by Augustine's MacBook on 3/11/20.
+//  Copyright © 2020 Augustine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
